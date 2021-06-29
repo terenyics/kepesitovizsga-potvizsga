e171a862cfde7a15914b4bf99ba1ae3b63598864
terenyicsaba <terenyicsaba@gmail.com>
2021-06-29 09:57:16 +0200

    letter multiplier

---
+++ src/main/java/hu/nive/ujratervezes/LetterMultiplier.java
@@ -0,0 +1,19 @@
+package hu.nive.ujratervezes;
+
+public class LetterMultiplier {
+
+    public String multiply(String input, int number) {
+        if (input == null || input.equals("") || number < 0) {
+            throw new IllegalArgumentException("Unsupported argument provided.");
+        }
+
+        String output = "";
+
+        for (char letter : input.toCharArray()) {
+            for (int i=0; i<number; i++) {
+                output+=letter;
+            }
+        }
+        return output;
+    }
+}
